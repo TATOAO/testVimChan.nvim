7b858b3de375d19bea7be6f1cac7b534c9f9a03b
TATOAO <liangwentaoss@126.com>
2024-04-14 15:02:29 +0800

    create first commit try vimchan

---
+++ lua/testVimChan/init.lua
@@ -0,0 +1,50 @@
+
+local function start_job()
+    -- Callbacks to handle job output and exit
+    local on_stdout = function(job_id, data, event)
+        if data then
+            print("Output:", table.concat(data, "\n"))
+        end
+    end
+
+    local on_exit = function(job_id, exit_code, event)
+        print("Job", job_id, "exited with code", exit_code)
+    end
+
+    -- Setting up the jobstart options
+    local opts = {
+        on_stdout = on_stdout,
+        on_exit = on_exit,
+        stdout_buffered = true,
+    }
+
+	-- local cmd = {
+	-- 	"sql-fomratter"
+	-- }
+    -- The command to run
+    local command = "cat"
+
+    -- Start the job
+    local status, job_id_or_err = pcall(vim.fn.jobstart, command, opts)
+
+    if status then
+        print("Job started successfully with ID:", job_id_or_err)
+    else
+        print("Failed to start job:", job_id_or_err)
+    end
+
+	local jid = job_id_or_err
+	vim.api.nvim_chan_send(jid, "hello shit")
+	print(jid)
+	print('eeeeee')
+    vim.fn.chanclose(jid, "stdin")
+
+	vim.fn.jobstop(jid)
+
+
+end
+
+-- Execute the function
+start_job()
+
+
